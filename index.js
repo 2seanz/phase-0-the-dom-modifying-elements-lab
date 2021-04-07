e1b6335dc87cce41eddd83e25f45a1f36f37ea7f
Sean Ngo <seanzngo@gmail.com>
2021-04-07 17:54:56 -0400

    commit tree 2e05a819de3d179a67a864ed6d7b8d012c289e50

--- index.js
+++ index.js
@@ -1,13 +1,28 @@
 // Write your code here!
-document.querySelector('main').remove();
+// document.querySelector('main').remove();
 
-const newHeader = document.createElement('h1');
-document.body.appendChild(newHeader);
-newHeader.id = "victory";
-newHeader.innerHTML = "<h1>YOUR-NAME is the champion</h1>";
-
-
+// const newHeader = document.createElement('h1');
+// document.body.appendChild(newHeader);
+// newHeader.id = "victory";
+// newHeader.innerHTML = "<h1>YOUR-NAME is the champion</h1>";
 
 
 
 
+document.querySelector("main").remove()
+
+// const newHeader = document.createElement("h1")
+// newHeader.setAttribute('id', 'victory')
+
+// // const myName = document.createElement("h1")
+// myName.textContent = "YOUR-NAME is the champion"
+// newHeader.append(myName)
+
+
+// //newHeader.innerHTML = "<h1>YOUR-NAME is the champion</h1>"
+
+const newHeader = document.createElement("h1");
+newHeader.setAttribute("id","victory");
+const myName = document.createElement("p");
+myName.textContent = "Jamse-Bond  is the champion";
+newHeader.appendChild(myName);
